d55e4447e36c891ec687ce0f4db9303c096919f4
Артем <artem@MacBook-Pro-Artem.local>
2022-09-10 16:51:14 +0300

    initial

---
+++ src/test/java/ru/netology/web/CallBackTest.java
@@ -0,0 +1,54 @@
+package ru.netology.web;
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CallBackTest {
+    private WebDriver driver;
+
+    @BeforeAll  // если тестов несколько выполниться один раз перед запуском всех тестов
+    static void setUpAll(){
+        System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver");
+    }
+
+    @BeforeEach  // анатация будет выполняться перед каждым тестом
+    void setUp(){
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach  // запускается после каждого теста
+    void tearsDown(){  // закрываем браузер после каждого теста
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void test(){
+        driver.get("http://localhost:9999");
+
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+
+        elements.get(0).sendKeys("Иван Петров");
+        elements.get(1).sendKeys("+79211231223");
+
+        //driver.findElement().sendKeys("Иван Петров");
+        //driver.findElement().sendKeys("+79211231223");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button__text")).click();
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+    }
+
+}
