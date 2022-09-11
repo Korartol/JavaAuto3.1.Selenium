dccd52672a74a5b3e25cad48c62134793c3cbae1
Артем <artem@MacBook-Pro-Artem.local>
2022-09-11 12:47:24 +0300

    add headless

--- src/test/java/ru/netology/web/CallBackTest.java
+++ src/test/java/ru/netology/web/CallBackTest.java
@@ -1,6 +1,7 @@
 package ru.netology.web;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,6 +10,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
 
 import java.util.List;
 
@@ -17,24 +20,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class CallBackTest {
     private WebDriver driver;
 
+    @BeforeAll
+    static void setupAll() {
+        WebDriverManager.chromedriver().setup();
+    }
+
     @BeforeAll  // если тестов несколько выполниться один раз перед запуском всех тестов
-    static void setUpAll(){
+    static void setUpAll() {
         System.setProperty("webdriver.chrome.driver", "driver/mac/chromedriver");
     }
 
-    @BeforeEach  // анатация будет выполняться перед каждым тестом
-    void setUp(){
-        driver = new ChromeDriver();
+    @BeforeEach
+        // анатация будет выполняться перед каждым тестом
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+
+        driver = new ChromeDriver(options);
     }
 
-    @AfterEach  // запускается после каждого теста
-    void tearsDown(){  // закрываем браузер после каждого теста
+    @AfterEach
+        // запускается после каждого теста
+    void tearsDown() {  // закрываем браузер после каждого теста
         driver.quit();
         driver = null;
     }
 
     @Test
-    void test(){
+    void test() {
         driver.get("http://localhost:9999");
 
         List<WebElement> elements = driver.findElements(By.className("input__control"));
